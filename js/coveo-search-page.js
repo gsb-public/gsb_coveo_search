b382e61ccc051b3a8522dca303eb22db52765d4b
Greg Mercer (GBear) <gmercer@stanford.edu>
2018-04-12 14:17:18 -0700

    A little code cleanup

--- js/coveo-search-page.js
+++ js/coveo-search-page.js
@@ -5,56 +5,56 @@ var gBaseUrl = '';
 
 (function ($) {
 
-    Drupal.behaviors.gsb_feature_coveo_search_page = {
+  Drupal.behaviors.gsb_feature_coveo_search_page = {
+    attach: function (context, settings) {
+      gInQueryString = settings.gsb_feature_coveo_search_page.search_query;
+      gBaseUrl = settings.baseUrl;
+    }
+  };
 
-        attach: function (context, settings) {
-            gInQueryString = settings.gsb_feature_coveo_search_page.search_query;
-            gBaseUrl = settings.baseUrl;
-        }
-    };
-
-    Drupal.gsb_feature_coveo_search_page = Drupal.gsb_feature_coveo_search_page || {};
+  Drupal.gsb_feature_coveo_search_page = Drupal.gsb_feature_coveo_search_page || {};
 
 })(jQuery);
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var coveoOrgId = '';
-    var coveoSearchtoken = '';
-    var coveoProxyEndPoint = gBaseUrl + "/coveo-search/";
+  var coveoOrgId = '';
+  var coveoSearchtoken = '';
+  var coveoProxyEndPoint = gBaseUrl + "/coveo-search/";
 
-    Coveo.SearchEndpoint.configureCloudV2Endpoint(
-        coveoOrgId,
-        coveoSearchtoken,
-        coveoProxyEndPoint
-    );
+  Coveo.SearchEndpoint.configureCloudV2Endpoint(
+    coveoOrgId,
+    coveoSearchtoken,
+    coveoProxyEndPoint
+  );
+
+  var root = Coveo.$$(document).find('#search');
+
+  Coveo.init(root);
+
+  Coveo.$$(root).on('buildingQuery', function (e, args) {
+
+    var queryString = gInQueryString;
 
     var root = Coveo.$$(document).find('#search');
+    var qsModel = Coveo.state(root);
 
-    Coveo.init(root);
+    args.queryBuilder.enableDebug = true;
 
-    Coveo.$$(root).on('buildingQuery', function (e, args) {
+    if (qsModel.attributes.q == "" && gInQueryString != null && gInQueryString != "") {
+      gInQueryString = "";
+      qsModel.set("q", queryString);
+    }
+    
+  });
 
-        var queryString = gInQueryString;
+  Coveo.$$(root).on('querySuccess', function (e, args) {
+    var results = args.results.results;
+    console.log(results);
+  });
 
-        var root = Coveo.$$(document).find('#search');
-        var qsModel = Coveo.state(root);
-
-        args.queryBuilder.enableDebug = true;
-
-        if (qsModel.attributes.q == "" && gInQueryString != null && gInQueryString != "") {
-            gInQueryString = "";
-            qsModel.set("q", queryString);
-        }
-    });
-
-    Coveo.$$(root).on('querySuccess', function (e, args) {
-        var results = args.results.results;
-        console.log(results);
-    });
-
-    Coveo.$$(root).on('state:change:q', function (e, args) {
-        console.log('The new value of ' + args.attribute + ' is now : ' + args.value);
-    });
+  Coveo.$$(root).on('state:change:q', function (e, args) {
+    console.log('The new value of ' + args.attribute + ' is now : ' + args.value);
+  });
 
 });
