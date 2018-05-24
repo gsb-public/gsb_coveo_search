9f3d34dbd14aa8719a9b310151975cc4dce62a78
Greg Mercer <gmercer@stanford.edu>
2018-05-24 07:59:57 -0700

    Create coveo-search-suggests.js

---
+++ js/coveo-search-suggests.js
@@ -0,0 +1,48 @@
+// Globals
+
+var gInQueryString = '';
+var gBaseUrl = '';
+
+(function ($) {
+
+  Drupal.behaviors.gsb_feature_coveo_search_page = {
+    attach: function (context, settings) {
+      gBaseUrl = settings.baseUrl;
+    }
+  };
+
+  Drupal.gsb_feature_coveo_search_page = Drupal.gsb_feature_coveo_search_page || {};
+
+})(jQuery);
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  var coveoOrgId = '';
+  var coveoSearchtoken = '';
+  var coveoProxySearchEndPoint = gBaseUrl + "/coveo-search/";
+  var coveoProxyAnalyticsEndPoint = gBaseUrl + "/coveo-analytics/";
+
+  Coveo.SearchEndpoint.configureCloudV2Endpoint(
+    coveoOrgId,
+    coveoSearchtoken,
+    coveoProxySearchEndPoint
+  );
+
+  Coveo.Analytics.options.endpoint.defaultValue = coveoProxyAnalyticsEndPoint;
+  Coveo.Analytics.options.organization.defaultValue = coveoOrgId;
+
+  var root = Coveo.$$(document).find('#search');
+
+  Coveo.init(document.querySelector('#search'));
+
+  Coveo.$$(root).on('querySuccess', function (e, args) {
+    var results = args.results.results;
+    console.log(results);
+  });
+
+  Coveo.$$(root).on('state:change:q', function (e, args) {
+    window.location.replace(gBaseUrl + "/search/?#q=" + args.value);
+    console.log('The new ' + args.attribute + ' is now : ' + args.value);
+  });
+
+});
