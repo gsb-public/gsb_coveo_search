8dd4c931d8026bd7874b08c5d2f433a8aded8bea
tribloom <mmccarthy@tribloom.com>
2019-03-24 06:27:37 -0600

    Changing department facet limit to 12 to avoid '+' in facet widget.

--- theme/coveo-mygsb-search-page.tpl.php
+++ theme/coveo-mygsb-search-page.tpl.php
@@ -22,7 +22,7 @@
   <div class="coveo-main-section">
     <div class="coveo-facet-column">
         <div class="CoveoFacet" data-title="Source" data-field="@source" data-tab="All" data-custom-sort="mygsb website, stanford gsb public, stanford gsb youtube, gsb events, stanford events" data-enable-settings="false"></div>
-        <div class="CoveoFacet" data-title="Department" data-field="@department" data-tab="All" data-enable-settings="false" data-number-of-values="6"></div>
+        <div class="CoveoFacet" data-title="Department" data-field="@department" data-tab="All" data-enable-settings="false" data-number-of-values="12"></div>
     </div>
     <div class="coveo-results-column">
       <div class="CoveoShareQuery"></div>
