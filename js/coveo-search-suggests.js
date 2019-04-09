49e5234ec3eba2d9e64b58e0490f80a21b59384d
Greg Mercer <gmercer@stanford.edu>
2019-04-09 13:34:13 -0700

    Updating with changes from Michael (Tribloom)

--- js/coveo-search-suggests.js
+++ js/coveo-search-suggests.js
@@ -33,7 +33,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var root = Coveo.$$(document).find('#search');
 
-  Coveo.init(document.querySelector('#search'));
+  var sourceFacetValueCaption = {
+    "Stanford GSB public" : "GSB Public website",
+    "GSB Public website" : "GSB Public website",
+    "Stanford GSB YouTube" : "GSB YouTube"
+  };
+
+  Coveo.init(document.querySelector("#search"), {
+    sourceFilter : {
+      valueCaption : sourceFacetValueCaption
+    }
+  });
 
   Coveo.$$(root).on('querySuccess', function (e, args) {
     var results = args.results.results;
